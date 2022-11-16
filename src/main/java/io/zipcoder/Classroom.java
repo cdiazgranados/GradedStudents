b2d5b92ee39021f285af10d378a2238c0d2c865e
Carolina Diazgranados <linacello@gmail.com>
2022-11-15 23:25:10 -0500

    group build with calvin, andrew, collin, emory, and freddy

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,75 @@
 package io.zipcoder;
 
 public class Classroom {
+
+    private Student[] student;
+    private int maxNumberOfStudents;
+
+    public Classroom() {
+        student = new Student[30];
+    }
+
+    public Classroom(Student[] student) {
+        this.student = student;
+    }
+
+    public Classroom(int maxNumberOfStudents){
+        student = new Student[maxNumberOfStudents];
+    }
+
+    public Student[] getStudents() {
+        return this.student;
+    }
+
+    public void addStudent(Student student) {
+
+        for (int i = 0; i < this.student.length; i++) {
+            if (this.student[i] == null) {
+                this.student[i] = student;
+                break;
+            }
+        }
+    }
+
+    public double getAverageExamScore(){
+        double sum = 0;
+        double count = 0;
+
+        for (int i = 0; i < this.student.length; i++) {
+            if (this.student[i] != null) {
+                sum += this.student[i].getAverageExamScore();
+                count++;
+            }
+        }
+        return sum/count;
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        for (int i = 0; i < student.length; i++) {
+        if (student[i].getFirstName().equals(firstName) && student[i].getLastName().equals(lastName)) {
+            student[i] = null;
+        }
+        boolean sorted = false;
+        Student swap;
+
+        for (int j = 0; j < student.length - 1; j++) {
+            if (student[j].getFirstName().equals(firstName) && student[j].getLastName().equals(lastName)) {
+                swap = student[j];
+                student[j] = student[j + 1];
+                student[j+1] = swap;
+                sorted = false;
+            }
+
+        }
+    }
+
+
+//    public Student[] getStudentByScore(){
+//        return null;
+//    }
+
+//    public ... getGradeBook(){
+//        return null;
+    }
+
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,98 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ClassroomTest {
+
+    @Test
+    public void addStudent(){
+
+
+        // : Given
+        int maxNumberOfStudents = 1;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        ArrayList<Double> examScoreList = new ArrayList<>(List.of(examScores));
+        Student student = new Student("Leon", "Hunter", examScoreList);
+
+        // When
+        Student[] preEnrollment = classroom.getStudents();
+        String preEnrollmentAsString = Arrays.toString(preEnrollment);
+        classroom.addStudent(student);
+        Student[] postEnrollment = classroom.getStudents();
+
+        // Then
+
+        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+
+        System.out.println("===========================");
+        System.out.println(preEnrollmentAsString);
+        System.out.println("===========================");
+        System.out.println(postEnrollmentAsString);
+
+//        Assert.assertEquals();
+
+    }
+
+    @Test
+    public void getAverageExamScore() {
+        // : Given
+        Double[] s1Scores = {100.0, 150.0};
+        Double[] s2Scores = {225.0, 25.0};
+
+        ArrayList<Double> examScoreList1 = new ArrayList<>(List.of(s1Scores));
+        ArrayList<Double> examScoreList2 = new ArrayList<>(List.of(s2Scores));
+
+        Student s1 = new Student("student", "one", examScoreList1);
+        Student s2 = new Student("student", "two", examScoreList2);
+
+        Student[] students = {s1, s2};
+        Classroom classroom = new Classroom(students);
+
+        // When
+        double output = classroom.getAverageExamScore();
+
+        // Then
+        System.out.println(output);
+    }
+    @Test
+    public void removeStudent() {
+
+        // : Given
+        Double[] s1Scores = {100.0, 150.0};
+        Double[] s2Scores = {225.0, 25.0};
+        Double[] s3Scores = {100.0, 150.0};
+        Double[] s4Scores = {225.0, 25.0};
+
+        ArrayList<Double> examScoreList1 = new ArrayList<>(List.of(s1Scores));
+        ArrayList<Double> examScoreList2 = new ArrayList<>(List.of(s2Scores));
+        ArrayList<Double> examScoreList3 = new ArrayList<>(List.of(s3Scores));
+        ArrayList<Double> examScoreList4 = new ArrayList<>(List.of(s4Scores));
+
+        Student s1 = new Student("student", "one", examScoreList1);
+        Student s2 = new Student("student", "two", examScoreList2);
+        Student s3 = new Student("student", "three", examScoreList3);
+        Student s4 = new Student("student", "four", examScoreList4);
+
+        Student[] students = {s1, s2, s3, s4};
+        Classroom classroom = new Classroom(students);
+
+        classroom.removeStudent("student", "two");
+
+        // When
+        Student[] actual = classroom.getStudents();
+        String removed = Arrays.toString(actual);
+        Student[] expected = {s1, s3, s4, null};
+
+        // Then
+        System.out.println(removed);
+        Assert.assertArrayEquals(actual, expected);
+    }
+
 }
